fa9e42573ce9386d01c65cc7ca1767b7ee1bebfb
Gustavo <gustavotobiasp@gmail.com>
2023-03-22 20:05:35 -0300

    Trabajos

---
+++ clase 3/ejercicio1/src/Ejercicio1_C.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Ejercicio1_C {
+
+	public static void main(String[] args) {
+		int vec[] = new int [] {4,9,6,3,8,7,15,46};
+		int x,suma=0;
+		
+		System.out.println("Ingrese un numero natural:\n");
+		Scanner leer = new Scanner (System.in);
+		x=leer.nextInt();
+		leer.close();
+		for(int i=0;i<vec.length;i++) {
+			if(vec[i]>x) {
+				suma+=vec[i];
+			}
+		}
+		System.out.println("la seuma de los numeros mayores que "+ x + " es: "+ suma);
+
+	}
+
+}
